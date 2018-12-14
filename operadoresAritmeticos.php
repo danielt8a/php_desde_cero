41a29fa18a153051c74935be03737fef17cc3bcd
DamasoDaniel <damasodt8a@gmail.com>
2018-12-14 16:51:26 -0600

    Operadores de asignación

--- operadoresAritmeticos.php
+++ operadoresAritmeticos.php
@@ -1,4 +1,52 @@
 <?php
-	
+/*	Operadores aritméticos
+
+	Suma +, suma de a + b
+	Resta -, diferencia de a - b
+	Multiplicación *, producto de a * b
+	División /, cociente de a y b
+	Módulo %, resto de a dividido por b
+*/	
+	$a = 10;
+	$b = 20;
+
+	//Suma
+	$suma = $a + $b;
+
+	echo "Suma: ".$suma."<br>";
+
+
+	$resta = $a - $b;
+
+	echo "Resta: ".$resta."<br>";
+
+
+	$multiplicacion = $a * $b;
+
+	echo "multiplicación: ".$multiplicacion."<br>";
+
+	$division = $a / $b;
+
+	echo "division: ".$division."<br>";
+
+	$modulo = $b / $a;
+
+	echo "modulo: ".$modulo."<br>";
+
+	/*Operadores de asignación
+
+	=, += *= /= .=*/
+
+	$a = 10;
+	$a += 5;
+
+	echo $a."<br>";
+
+	$c = 30;
+
+	$c *= 5;
+
+	echo $c;
+
 
 ?>
