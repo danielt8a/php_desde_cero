ec3ed3c22ada6bcaba7ceddd4d361ee45589cb2c
DamasoDaniel <damasodt8a@gmail.com>
2018-12-14 21:08:19 -0600

    Operadores de incremento y decremento

---
+++ incrementoDecremento.php
@@ -0,0 +1,11 @@
+<?php
+	/*Operadores de incremento / decremento
+
+	++$variable , $variable++
+	--$variable , $variable--*/
+
+	$valor = 15;
+	$nuevoValor = $valor++;
+	echo $nuevoValor;
+	echo $valor;
+?>
