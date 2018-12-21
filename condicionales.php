70f8e4c411231599451b2769c3f78f4d8e07f098
DamasoDaniel <damasodt8a@gmail.com>
2018-12-20 23:52:21 -0600

    Operador if else

--- condicionales.php
+++ condicionales.php
@@ -1,4 +1,26 @@
 <?php
+	//Condición If else
 	
+	/**
+	 * if (condicion) {
+		bloque de código
+	 } else {
 	
+	 }
+	 */
+
+	 $numero = 2;
+
+	 if ($numero == 1) {
+	 	echo "El número es igual a 1 <br>";
+	 } else  if($numero == 2) {
+	 	echo "El número es igual a 2 <br>";
+	 } else {
+	 	echo "Es otro número <br>";
+	 }
+
+	 // Operador ternario
+
+
+
 ?>
